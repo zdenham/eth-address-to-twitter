1e13c3f58c051a999292259a75cb4bf90ebddedd
zdenham <zdenham1@gmail.com>
2022-09-01 22:26:26 -0700

    serving a webpage

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
 import express from 'express';
+
+const app = express();
+
+const port = 4000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
